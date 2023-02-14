6dd90a82f995457c90278e8796ad2294ff955533
Andrey Amelin <more207@mail.ru>
2023-02-14 20:24:12 +0300

    fix: добавь адрес в корс

--- constants/index.js
+++ constants/index.js
@@ -1,7 +1,7 @@
 const CAST_ERROR = 'CastError';
 const REGEX_URL = /https?:\/\/(www\.)?[-a-zA-Z0-9:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_.~#?&//=]*)/;
 const VALIDATION_ERROR = 'Validation failed';
-const allowedCors = ['https://51.250.100.102', 'http://51.250.100.102', 'http://localhost:3000'];
+const allowedCors = ['https://amelin.mesto.nomoredomains.icu/', 'http://localhost:3000'];
 
 module.exports = {
   CAST_ERROR,
