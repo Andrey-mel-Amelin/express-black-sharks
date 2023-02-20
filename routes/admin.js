c668d29b0636cef4f5fc921e06174a8eacb85a0d
Andrey Amelin <more207@mail.ru>
2023-02-20 22:49:18 +0300

    fix: опечатка

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { login, logout, getAdmin } = require('../controllers/admin');
 
-router.get('login', getAdmin);
+router.get('/login', getAdmin);
 router.post('/login', login);
 router.delete('/logout', logout);
 
