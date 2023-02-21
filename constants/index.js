5c668f8531560c673442f3dd3c1948e312752316
Andrey Amelin <more207@mail.ru>
2023-02-22 02:44:58 +0300

    fix: добавить адреса для сохранения изображений

--- constants/index.js
+++ constants/index.js
@@ -1,9 +1,14 @@
 const CAST_ERROR = 'CastError';
 const VALIDATION_ERROR = 'Validation failed';
 const allowedCors = ['https://amelin.mesto.nomoredomains.icu', 'http://localhost:3000'];
+const backendUrl = {
+  local: 'http://localhost:3001',
+  deploy: 'https://amelin.mesto.backend.nomoredomains.icu',
+};
 
 module.exports = {
   CAST_ERROR,
   VALIDATION_ERROR,
   allowedCors,
+  backendUrl,
 };
